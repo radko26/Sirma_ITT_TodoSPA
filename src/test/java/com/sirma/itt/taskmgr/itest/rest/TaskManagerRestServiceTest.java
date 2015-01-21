2d47586ef4d9dfe01d4c8e0d0e0ffc9abaeb0fee
Radoslav Petkov <radoslav.petkov@sirma.bg>
2015-01-21 17:52:31 +0200

    sonar support

--- src/test/java/com/sirma/itt/taskmgr/itest/rest/TaskManagerRestServiceTest.java
+++ src/test/java/com/sirma/itt/taskmgr/itest/rest/TaskManagerRestServiceTest.java
@@ -1,23 +1,14 @@
 package com.sirma.itt.taskmgr.itest.rest;
 
-import javax.ws.rs.core.MediaType;
-import static org.junit.Assert.*;
-import org.hamcrest.Matchers;
-import org.json.JSONObject;
-import org.junit.Before;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Response;
 import com.sirma.itt.taskmgr.model.TaskBuilder;
 import com.sirma.itt.taskmgr.util.TaskDB;
 
 public class TaskManagerRestServiceTest {
 
-	@Before
-	public void setup() {
-		RestAssured.baseURI = "http://localhost:8080/SirmaTaskManager/rest";
-	}
 
 	@Test
 	public void testAddTask() {
@@ -25,23 +16,12 @@ public class TaskManagerRestServiceTest {
 		TaskDB db = new TaskDB();
 		db.insert(new TaskBuilder().setContent("as").build());
 		assertTrue(true);
-		/*
-		 * JSONObject task = new JSONObject(); task.put("finished", true); task.put("content",
-		 * "Test it!"); task.put("expire", 0); Response response =
-		 * RestAssured.given().contentType(MediaType.APPLICATION_JSON).body(task.toString())
-		 * .when().post("/tasks/create");
-		 * RestAssured.expect().statusCode(200).contentType(MediaType.APPLICATION_JSON).when()
-		 * .get("/tasks/").then().contentType(MediaType.APPLICATION_JSON).body("content",
-		 * Matchers.equalTo("[Test it!]"));
-		 */
 	}
 
 	@Test
 	public void testGetTasks() {
-		/*
-		 * RestAssured.expect().statusCode(200).contentType(MediaType.APPLICATION_JSON).when()
-		 * .get("/tasks/");
-		 */
+
+		
 	}
 
 }
