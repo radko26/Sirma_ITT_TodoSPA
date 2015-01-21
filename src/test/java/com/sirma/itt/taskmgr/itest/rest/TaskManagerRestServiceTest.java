ee7d55390e256f3a5f7a872ca67af09258c0154d
Radoslav Petkov <radoslav.petkov@sirma.bg>
2015-01-21 17:47:31 +0200

    update

--- src/test/java/com/sirma/itt/taskmgr/itest/rest/TaskManagerRestServiceTest.java
+++ src/test/java/com/sirma/itt/taskmgr/itest/rest/TaskManagerRestServiceTest.java
@@ -1,19 +1,16 @@
 package com.sirma.itt.taskmgr.itest.rest;
 
 import javax.ws.rs.core.MediaType;
-
+import static org.junit.Assert.*;
 import org.hamcrest.Matchers;
-import org.jboss.logging.MDC;
 import org.json.JSONObject;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.jayway.restassured.*;
-import com.jayway.restassured.internal.ResponseSpecificationImpl.HamcrestAssertionClosure;
-import com.jayway.restassured.matcher.RestAssuredMatchers;
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import com.sirma.itt.taskmgr.model.TaskBuilder;
+import com.sirma.itt.taskmgr.util.TaskDB;
 
 public class TaskManagerRestServiceTest {
 
@@ -24,20 +21,19 @@ public class TaskManagerRestServiceTest {
 
 	@Test
 	public void testAddTask() {
-		JSONObject task = new JSONObject();
-		task.put("finished", true);
-		task.put("content", "Test it!");
-		task.put("expire", 0);
 
+		TaskDB db = new TaskDB();
+		db.insert(new TaskBuilder().setContent("as").build());
+		assertTrue(true);
 		/*
-		 * Response response =
+		 * JSONObject task = new JSONObject(); task.put("finished", true); task.put("content",
+		 * "Test it!"); task.put("expire", 0); Response response =
 		 * RestAssured.given().contentType(MediaType.APPLICATION_JSON).body(task.toString())
 		 * .when().post("/tasks/create");
 		 * RestAssured.expect().statusCode(200).contentType(MediaType.APPLICATION_JSON).when()
 		 * .get("/tasks/").then().contentType(MediaType.APPLICATION_JSON).body("content",
 		 * Matchers.equalTo("[Test it!]"));
 		 */
-
 	}
 
 	@Test
